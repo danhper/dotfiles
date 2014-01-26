cf09d484beea8d285c542ea88a3ed2e2aff0a62b
Daniel Perez <tuvistavie@gmail.com>
2014-01-26 19:51:51 +0900

    Add multidisplay configuration for xmonad.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,6 +4,7 @@ import XMonad.Util.CustomKeys
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.IndependentScreens
 import qualified XMonad.StackSet as W
 
 keysBindings :: [(String, X())]
@@ -18,6 +19,12 @@ keysBindings =
   , ("<Print>"  , spawn "xwd | convert - /tmp/screenshot-$(date +%s).png")
   , ("C-<Print>", spawn "xwd -root | convert - /tmp/screenshot-$(date +%s).png")
   ]
+  ++
+  [
+    (mask ++ "M4-" ++ [key], screenWorkspace scr >>= flip whenJust (windows . action))
+         | (key, scr)  <- zip "asd" [2,0,1] -- was [0..] *** change to match your screen order ***
+         , (action, mask) <- [ (W.view, "") , (W.shift, "S-")]
+  ]
 
 keyUnbindings :: [String]
 keyUnbindings =
@@ -25,19 +32,22 @@ keyUnbindings =
     "M-h"
   , "M-l"
   ] ++ concat [("M-" ++ [n]):["M-S-" ++ [n]] | n <- ['1'..'9']]
+    ++ concat [("M-" ++ [c]):["M-S-" ++ [c]] | c <- "wer"]
 
 mCustomKeys = customKeys delkeys inskeys where
   delkeys :: XConfig l -> [(KeyMask, KeySym)]
   delkeys XConfig {modMask = modm} = []
   inskeys :: XConfig Layout -> [((KeyMask, KeySym), X ())]
   inskeys conf@(XConfig {XMonad.modMask = modMask}) =
-    [((m .|. mod4Mask, k), windows $ f i)
-          | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-          , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+    [((m .|. mod4Mask, k), windows $ onCurrentScreen f i)
+          | (i, k) <- zip (workspaces' conf) [xK_1 .. xK_9]
+          , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 manageHooks = composeOne [isFullscreen -?> doFullFloat]
 
-mainConfig = defaultConfig
+workspaceNames = [show n | n <- [1..9]]
+
+mainConfig n = defaultConfig
   { terminal           = "terminator"
   , normalBorderColor  = "#cccccc"
   , focusedBorderColor = "#7ec7ea"
@@ -46,15 +56,18 @@ mainConfig = defaultConfig
   , keys               = mCustomKeys
   , startupHook        = setWMName "LG3D"
   , manageHook         = manageHooks
+  , workspaces         = withScreens n workspaceNames
 }
 
-configWithKeys = mainConfig `removeKeysP` keyUnbindings
-                            `additionalKeysP` keysBindings
+configWithKeys n = mainConfig n `removeKeysP` keyUnbindings
+                                `additionalKeysP` keysBindings
 
-barCommand = "xmobar /home/daniel/.xmobar/xmobarrc"
+barCommand = "xmobar -x1 /home/daniel/.xmobar/xmobarrc"
 myPP = xmobarPP
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_z)
 
-configWithBar = statusBar barCommand myPP toggleStrutsKey configWithKeys
+configWithBar n = statusBar barCommand myPP toggleStrutsKey (configWithKeys n)
 
-main = xmonad =<< configWithBar
+main = do
+  screensNumber <- countScreens
+  xmonad =<< configWithBar screensNumber
