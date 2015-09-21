e28ddb97cbd9b57a695eb9034a0a4392c0435517
Daniel Perez <daniel@claudetech.com>
2015-09-21 13:09:18 +0900

    Add plugins directory.

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,11 +2,17 @@ if test -z "$TMUX"
   exec tmux
 end
 
+set plugins_directory $HOME/.config/fish/plugins
+
 set fish_greeting
 
 source $HOME/.config/fish/settings/prompt.fish
 source $HOME/.config/fish/settings/completion_helpers.fish
 source $HOME/.config/fish/settings/ssh-agent.fish
 source $HOME/.config/fish/settings/aliases.fish
+
+for file in (ls $plugins_directory)
+  source "$plugins_directory/$file"
+end
 
 source "$HOME/.homesick/repos/homeshick/homeshick.fish"
---
+++ home/.config/fish/plugins/archlinux.fish
@@ -0,0 +1,1 @@
+alias yaupg='yaourt -Syu'
