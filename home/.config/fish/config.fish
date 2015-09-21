b9bf8b40e47ab816aa0acd29b12c3bf0c30e2c0b
Daniel Perez <daniel@claudetech.com>
2015-09-21 12:38:14 +0900

    Add fish completions.

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -5,6 +5,7 @@ end
 set fish_greeting
 
 source $HOME/.config/fish/settings/prompt.fish
+source $HOME/.config/fish/settings/completion_helpers.fish
 source $HOME/.config/fish/settings/ssh-agent.fish
 source $HOME/.config/fish/settings/aliases.fish
 
