c45dd14b2c79915bf2bd3a0e787800268b1f01f2
Daniel Perez <daniel@claudetech.com>
2015-11-10 15:19:16 +0900

    Add gcalcli settings.

--- home/.config/fish/plugins.fish
+++ home/.config/fish/plugins.fish
@@ -2,6 +2,7 @@ fundle plugin 'tuvistavie/fish-fastdir'
 fundle plugin 'tuvistavie/fish-completion-helpers'
 fundle plugin 'tuvistavie/fish-ssh-agent'
 fundle plugin 'tuvistavie/fish-theme-afowler'
+fundle plugin 'tuvistavie/fish-gcalcli'
 
 fundle plugin 'edc/bass'
 fundle plugin 'tuvistavie/fish-nvm'
