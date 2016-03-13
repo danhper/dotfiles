a09110340e45b1540f4a62cd97b311f79da88537
Daniel Perez <daniel@claudetech.com>
2016-03-13 17:10:26 +0900

    Update bindings in helm and company modes.

--- home/.spacemacs.d/tuvistavie/packages.el
+++ home/.spacemacs.d/tuvistavie/packages.el
@@ -1,31 +1,4 @@
 ;;; packages.el --- tuvistavie layer packages file for Spacemacs.
-;;
-;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
-;;
-;; Author:  <daniel@leffe>
-;; URL: https://github.com/syl20bnr/spacemacs
-;;
-;; This file is not part of GNU Emacs.
-;;
-;;; License: GPLv3
-
-;;; Commentary:
-
-;; See the Spacemacs documentation and FAQs for instructions on how to implement
-;; a new layer:
-;;
-;;   SPC h SPC layers RET
-;;
-;;
-;; Briefly, each package to be installed or configured by this layer should be
-;; added to `tuvistavie-packages'. Then, for each package PACKAGE:
-;;
-;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
-;;   function `tuvistavie/init-PACKAGE' to load and initialize the package.
-
-;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
-;;   define the functions `tuvistavie/pre-init-PACKAGE' and/or
-;;   `tuvistavie/post-init-PACKAGE' to customize the package as it is loaded.
 
 ;;; Code:
 
@@ -36,8 +9,11 @@
     web-mode
     js2-mode
     wakatime-mode
+    company
     projectile
-    feature-mode))
+    feature-mode
+    helm
+    neotree))
 
 (defun tuvistavie/init-editorconfig ()
   (use-package editorconfig
@@ -74,6 +50,19 @@
 (defun tuvistavie/post-init-projectile ()
   (setq projectile-enable-caching t)
   (setq projectile-indexing-method 'alien))
+
+(defun tuvistavie/post-init-helm ()
+  (with-eval-after-load 'helm
+    (define-key helm-map (kbd "C-h") 'delete-backward-char)))
+
+(defun tuvistavie/post-init-neotree ()
+  (setq neo-theme 'nerd))
+
+(defun tuvistavie/post-init-company ()
+  (with-eval-after-load 'company
+    (define-key company-active-map (kbd "C-h") 'delete-backward-char)
+    (define-key company-active-map (kbd "C-n") 'company-select-next)
+    (define-key company-active-map (kbd "C-p") 'company-select-previous)))
 
 (defun tuvistavie/init-wakatime-mode ()
   (use-package wakatime-mode
