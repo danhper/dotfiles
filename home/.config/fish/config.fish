e3fc36e3687d677ba117560157f45a24a319d94e
Daniel Perez <tuvistavie@gmail.com>
2017-09-04 17:11:39 +0900

    Simplify fish config

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,14 +1,4 @@
-set confd_dir "$HOME/.config/fish/conf.d"
-
 set fish_greeting
-
-source $HOME/.config/fish/settings/aliases.fish
-
-source $HOME/.config/fish/plugins.fish
-
-for file in (ls "$confd_dir")
-    source "$confd_dir/$file"
-end
 
 set -l sourced \
     "$HOME/.homesick/repos/homeshick/homeshick.fish"
