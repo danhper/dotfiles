b5d6a2c5c6540b3d40e47cf188de39c823656d55
Daniel Perez <tuvistavie@gmail.com>
2017-10-19 22:39:48 +0900

    Replace pnmixer by volumeicon

--- home/.xmonad/autostart.sh
+++ home/.xmonad/autostart.sh
@@ -3,17 +3,17 @@ launch() {
 }
 
 if [ "$CURRENT_ENV" = "lab" -o "$CURRENT_ENV" = "home-desktop" ]; then
-    launch "trayer --edge top --align left --margin 1728 --SetDockType true --SetPartialStrut true --expand true --width 192 --widthtype pixel --transparent true --tint 0x000000 --height 22 --alpha 0"
+    launch "trayer --edge top --align left --margin 1728 --SetDockType true --SetPartialStrut true --expand true --width 192 --widthtype pixel --transparent true --tint 0x000000 --height 23 --alpha 0"
 elif [ "$CURRENT_ENV" = "lab-desktop" ]; then
     launch "trayer --edge top --align left --margin 2304 --SetDockType true --SetPartialStrut true --expand true --width 256 --widthtype pixel --transparent true --tint 0x000000 --height 23 --alpha 0"
 else
-    launch "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 192 --widthtype pixel --transparent true --tint 0x000000 --height 22 --alpha 0"
+    launch "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 192 --widthtype pixel --transparent true --tint 0x000000 --height 23 --alpha 0"
 fi
 
 launch "devmon"
 launch "xcompmgr -c -C -t-5 -l-5 -r4.2 -o.55"
 launch "feh --bg-scale ~/.background/amami.jpg"
-launch "pnmixer"
+launch "volumeicon"
 launch "xautolock -time 15 -locker slimlock"
 launch "pulseaudio --start"
 launch "telegram-desktop -startintray"
