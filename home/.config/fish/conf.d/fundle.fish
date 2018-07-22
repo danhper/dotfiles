c42212ad5744d64b8a1e03f8177201d4867bf8f0
Daniel Perez <tuvistavie@gmail.com>
2018-07-22 19:22:13 +0900

    Add pubs completions

--- home/.config/fish/conf.d/fundle.fish
+++ home/.config/fish/conf.d/fundle.fish
@@ -7,5 +7,6 @@ fundle plugin 'joehillen/to-fish'
 fundle plugin 'oh-my-fish/plugin-peco'
 fundle plugin 'tuvistavie/fish-asdf'
 fundle plugin 'tuvistavie/fish-kubectl'
+fundle plugin 'tuvistavie/fish-pubs'
 
 fundle init
