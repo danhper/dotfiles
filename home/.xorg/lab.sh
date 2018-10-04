9bd418a4dc4df8644df4ae50b1e1b97055bf18fb
Daniel Perez <tuvistavie@gmail.com>
2018-10-04 19:24:49 +0100

    Update settings for new lab

--- home/.xorg/lab.sh
+++ home/.xorg/lab.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-xrandr | grep -q "VGA1 connected"
-vga_connected=$?
-
-if [ $vga_connected -eq 0 ]; then
-    xrandr --output HDMI1 --mode 2560x1440 --primary --output eDP1 --mode 1920x1080 --left-of HDMI1 --output VGA1 --mode 1200x1600 --right-of HDMI1
+external_display=$(xrandr | grep -E ' connected' | grep -v eDP1 | cut -f1 -d' ')
+if [ -z "$external_display"  ]; then
+    xrandr --auto
 else
-    xrandr --output HDMI1 --mode 2560x1440 --primary --output eDP1 --mode 1920x1080 --left-of HDMI1
+    xrandr --output "$external_display" --mode 1920x1200  --primary --output eDP1 --mode 1920x1080  --right-of "$external_display"
 fi
