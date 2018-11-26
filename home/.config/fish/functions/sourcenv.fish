a345e71128f4871c3af8e110e2c825b5aefb3c1d
Daniel Perez <daniel@perez.sh>
2018-11-26 15:53:39 +0100

    Improve sourcenv.fish

--- home/.config/fish/functions/sourcenv.fish
+++ home/.config/fish/functions/sourcenv.fish
@@ -1,3 +1,3 @@
 function sourcenv -a file
-	cat "$file" | sed -e 's/\(^\|export \)/set -xg /' -e 's/=/ /' | source
+	cat "$file" | sed -e 's/\(^\|export \)/set -xg /' -e 's/=/ /' -e 's/\${/{$/g' | source
 end
