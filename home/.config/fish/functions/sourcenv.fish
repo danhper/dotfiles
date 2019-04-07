690f45e0fa399a514919fc7a4b8f53c9982ae801
Daniel Perez <daniel@perez.sh>
2019-04-08 00:10:41 +0100

    Update sourcenv

--- home/.config/fish/functions/sourcenv.fish
+++ home/.config/fish/functions/sourcenv.fish
@@ -1,3 +1,3 @@
 function sourcenv -a file
-	cat "$file" | sed -e 's/\(^\|export \)/set -xg /' -e 's/=/ /' -e 's/\${/{$/g' | source
+	cat "$file" | grep -v '^\s*#' | sed -e 's/\(^\|export \)/set -xg /' -e 's/=/ /' -e 's/\${/{$/g' | source
 end
