fc1c04fc29c94cd993e045b0f3387059c792941e
Daniel Perez <daniel@perez.sh>
2019-04-23 18:05:32 +0100

    Move back to firefox

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -62,7 +62,7 @@ makeKeyBindings :: IO [(String, X())]
 makeKeyBindings = do
   currentEnv <- getCurrentEnv
   return ([
-      ("C-M1-f"   , spawn "brave-dev")
+      ("C-M1-f"   , spawn "firefox-developer-edition")
     , ("C-M1-t"   , spawn "urxvt")
     , ("C-M1-ç"   , spawn "ibus engine mozc-jp; sleep 0.3 && sh ~/.xprofile setup_keyboard")
     , ("C-M1-9"   , spawn "ibus engine mozc-jp; sleep 0.3 && sh ~/.xprofile setup_keyboard")
