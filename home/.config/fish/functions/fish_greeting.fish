dcb9e6f89010b12e47d946e4de936a3e52daa45c
Daniel Perez <daniel@perez.sh>
2019-12-01 19:33:29 +0000

    Update flashcards settings

--- home/.config/fish/functions/fish_greeting.fish
+++ home/.config/fish/functions/fish_greeting.fish
@@ -1,5 +1,5 @@
 function fish_greeting
-    if command -s flashcards > /dev/null 2>&1
+    if test -z "$DISABLE_FLASHCARDS" && command -s flashcards > /dev/null 2>&1
         flashcards show -random
     end
 end
