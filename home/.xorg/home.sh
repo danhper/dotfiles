ad2c950face32fd062ba4fd5b4794ba008276a4d
Daniel Perez <daniel@perez.sh>
2020-05-03 15:39:49 +0200

    Update home xorg

--- home/.xorg/home.sh
+++ home/.xorg/home.sh
@@ -2,7 +2,7 @@
 
 external_display=$(xrandr | grep -E ' connected' | grep -v eDP1 | cut -f1 -d' ')
 if [ -z "$external_display" ]; then
-    xrandr --auto
+    sh $HOME/.xorg/laptop.sh
 else
-    xrandr --output $external_display --mode 1920x1080 --primary  --output eDP1 --mode 1920x1080 --right-of $external_display
+    xrandr --output $external_display --mode 1920x1080 --primary  --output eDP1 --mode 3840x2400 --left-of $external_display --scale 0.5x0.5
 fi
