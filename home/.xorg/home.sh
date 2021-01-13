c2b752b6bb9d6cf4c7d5df253da8eb11a105bf5b
Daniel Perez <daniel@perez.sh>
2021-01-13 02:03:15 +0000

    Remvoe .xorg/home.sh

--- home/.xorg/home.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-xrandr --output DP-3-2 --primary --mode 1920x1080
-xrandr --output eDP-1 --mode 3840x2400 --scale 0.5x0.5 --left-of DP-3-2
-xrandr --output DP-3-3 --mode 1920x1080 --right-of DP-3-2
