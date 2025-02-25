673b01c9ce7521d35cdc00a83510bf2392a58ace
Daniel Perez <daniel@perez.sh>
2025-02-25 22:31:23 +0000

    Improve sourcenv

--- home/.config/fish/functions/sourcenv.fish
+++ home/.config/fish/functions/sourcenv.fish
@@ -1,6 +1,6 @@
 function sourcenv -a file
-	if test -z "$file"
-		set file .env
-	end
-	cat "$file" | grep -v '^\s*#' | sed -r -e 's/(^|export )/set -xg /' -e 's/=/ /' -e 's/\${/{$/g' | source
+    if test -z "$file"
+        set file .env
+    end
+    cat "$file" | grep -v '^\s*$' | grep -v '^\s*#' | sed -r -e 's/(^|export )/set -xg /' -e 's/=/ /' | source
 end
