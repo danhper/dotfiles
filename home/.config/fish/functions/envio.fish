c7bc6b627c288a0c09ed7904e442e601d807c0ca
Daniel Perez <daniel@perez.sh>
2025-02-26 02:04:42 +0000

    Improve envio wrapper

--- home/.config/fish/functions/envio.fish
+++ home/.config/fish/functions/envio.fish
@@ -1,5 +1,5 @@
 function envio
-    if contains -- --help $argv; or contains -- -h $argv
+    if test (count $argv) -eq 0; or contains -- --help $argv; or contains -- -h $argv
         command envio $argv
         return
     end
@@ -9,7 +9,7 @@ function envio
         set profile (cat .envio)
     end
 
-    if test (count $argv) -ge 1; and test $argv[1] = list; and not contains -- -n $argv; and not contains -- --profile-name $argv; and not contains -- -p $argv; and not contains -- --profiles $argv
+    if test $argv[1] = list; and not contains -- -n $argv; and not contains -- --profile-name $argv; and not contains -- -p $argv; and not contains -- --profiles $argv
         command envio list -n $profile $argv[2..-1]
         return
     end
@@ -19,8 +19,11 @@ function envio
         return
     end
 
-    if test (count $argv) -eq 2; and test $argv[1] = create
-        command envio create $argv[2] -g 1FEFF6D65203687AC33771CDA0B4CCFEBD33E18A
+    if test $argv[1] = create
+        if test (count $argv) -eq 2
+            set profile $argv[2]
+        end
+        command envio create $profile -g 1FEFF6D65203687AC33771CDA0B4CCFEBD33E18A
         return
     end
 
@@ -29,7 +32,7 @@ function envio
         return
     end
 
-    if test (count $argv) -ge 1; and test $argv[1] = add; and not contains -- --envs $argv
+    if test $argv[1] = add; and not contains -- --envs $argv
         if string match '*=*' $argv[2] >/dev/null
             command envio add $profile --envs $argv[2]
         else
@@ -39,8 +42,21 @@ function envio
         return
     end
 
-    if test (count $argv) -ge 1; and test $argv[1] = remove; and not contains -- -e $argv; and not contains -- --envs-to-remove $argv
+    if test $argv[1] = remove; and not contains -- -e $argv; and not contains -- --envs-to-remove $argv
         command envio remove $profile --envs-to-remove $argv[2]
+        return
+    end
+
+    if test $argv[1] = destroy
+        if test (count $argv) -eq 2
+            set profile $argv[2]
+        end
+        set -l filepath ~/.envio/profiles/$profile.env
+        if not test -f $filepath
+            echo "Profile $profile does not exist"
+            return 1
+        end
+        rm $filepath
         return
     end
 
