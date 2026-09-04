7180ee62cf5ba6c5aaed4c9b5d2bfafc4a9e2ba3
Daniel Perez <daniel@perez.sh>
2026-09-04 14:47:14 +0100

    Only use tty pinentry in interactive mode

--- home/.config/fish/conf.d/gpg.fish
+++ home/.config/fish/conf.d/gpg.fish
@@ -1,1 +1,14 @@
-set -xg GPG_TTY (tty)
+if status is-interactive
+    set -e PINENTRY_USER_DATA
+    set -xg GPG_TTY (tty)
+
+    function gpg --wraps gpg
+        set -lx PINENTRY_USER_DATA TTY=1
+        command gpg $argv
+    end
+
+    function git --wraps git
+        set -lx PINENTRY_USER_DATA TTY=1
+        command git $argv
+    end
+end
